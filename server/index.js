5936be4adbb4e6bd75b1677070d3b90939587f0b
Anton Kokorev <antonkokorev114@gmail.com>
2022-02-16 17:44:46 +0300

    Added `GetAllWorkItems` request

--- server/index.js
+++ server/index.js
@@ -6,22 +6,42 @@ const treeBuilder = require('./utils/treeBuilder');
 const inquireService = require('./services/inquireService');
 const workItemsService = require('./services/workItemsService');
 
-clear();
+const express = require('express');
+const server = express();
+const port = 3000;
 
-console.log(chalk.yellow(figlet.textSync('Aims Project', { horizontalLayout: 'full' })));
+server.get('/work-items', (_, res) => {
+  workItemsService.getWorkItemsInfo()
+    .then(workItemsInfo => res.json(workItemsInfo))
+    .catch(error => res.send(error));
+});
 
-const displayData = workItemsTree => {
-  for (const tree of workItemsTree) {
-    printTree(tree, node => `${node.id} _ ${node.title}`, node => node.children);
-  }
-}
+server.get('/work-items/:workItemId', (req, res) => {
+  res.send(`Returns the work item with ID ${req.params.workItemId}`)
+});
 
-const run = () => {
-  inquireService.askAzureDevopsToken()
-    .then(() => workItemsService.getWorkItemsInfo())
-    .then(workItemsInfo => workItemsService.getWorkItemRelations(workItemsInfo))
-    .then(workItemsRelations => treeBuilder.buildTree(workItemsRelations))
-    .then(workItemsTree => displayData(workItemsTree));
-};
+server.listen(port, () => {
+  console.log(`Server app is listening on port ${port}`);
+});
 
-run();
+// clear();
+
+// console.log(chalk.yellow(figlet.textSync('Aims Project', { horizontalLayout: 'full' })));
+
+// const displayData = workItemsTree => {
+//   for (const tree of workItemsTree) {
+//     printTree(tree, node => `${node.id} _ ${node.title}`, node => node.children);
+//   }
+// }
+
+// const run = () => {
+//   inquireService.askAzureDevopsToken()
+//     .then(() => workItemsService.getWorkItemsInfo())
+//     .then(workItemsInfo => workItemsService.getWorkItemRelations(workItemsInfo))
+//     .then(workItemsRelations => treeBuilder.buildTree(workItemsRelations))
+//     .then(workItemsTree => displayData(workItemsTree));
+// };
+
+// run();
+
+
