b2fd3374c916528756d4365bb9f0b9c68d08aff5
Anton Kokorev <antonkokorev114@gmail.com>
2022-02-16 18:37:22 +0300

    Moved requests to `workItemsRouter`

--- server/index.js
+++ server/index.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const workItemsService = require('./services/workItemsService');
+const workItemsRouter = require('./routers/workItemsRouter');
 
 const server = express();
 const port = 3000;
 
-server.get('/work-items', (_, res) => {
-  workItemsService.getWorkItemsInfo()
-    .then(workItemsInfo => res.json(workItemsInfo))
-    .catch(error => res.send(error));
-});
-
-server.get('/work-items/:workItemId', (req, res) => {
-  workItemsService.getWorkItemById(req.params.workItemId)
-    .then(workItemInfo => res.json(workItemInfo))
-    .catch(error => res.send(error));
-});
+server.use('/work-items', workItemsRouter);
 
 server.listen(port, () => {
   console.log(`Server app is listening on port ${port}`);
