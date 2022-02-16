ab283e124f5b136d3f516a3ecd00e27104feb616
Anton Kokorev <antonkokorev114@gmail.com>
2022-02-16 14:27:13 +0300

    Updated API version

--- server/utils/urlBuilder.js
+++ server/utils/urlBuilder.js
@@ -1,7 +1,7 @@
 const serverUrl = 'https://dev.azure.com';
 const organization = '<ADO ORGANIZATION NAME>';
 const project = '<ADO PROJECT NAME WITH WORKITEMS>';
-const apiVersion = '5.1';
+const apiVersion = '6.0';
 
 module.exports.wiqlUrl = () => `${serverUrl}/${organization}/${project}/_apis/wit/wiql?api-version=${apiVersion}`;
 module.exports.workItemUrl = id => `${serverUrl}/${organization}/${project}/_apis/wit/workitems/${id}?api-version=${apiVersion}`;
